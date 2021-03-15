090052e378a82419f4cebe36f576923b37a85762
Laura <jlcunningham2@comcast.net>
2021-03-15 17:24:19 -0600

    First commit

---
+++ js/weather.js
@@ -0,0 +1,80 @@
+const form = document.querySelector(".weather-search form");
+const input = document.querySelector(".weather-search input");
+const msg = document.querySelector(".weather-search .msg");
+const list = document.querySelector(".location-section");
+
+/*https://home.openweathermap.org/*/
+const apiKey = "bd578d213152f33eaf1ebd4cade5161e";
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  let inputVal = input.value;
+
+  const listItems = list.querySelectorAll(".location-section cities");
+  const listItemsArray = Array.from(listItems);
+
+  if (listItemsArray.length > 0) {
+    const filteredArray = listItemsArray.filter(el => {
+      let content = "";
+      
+      if (inputVal.includes(",")) {
+        
+        if (inputVal.split(",")[1].length > 2) {
+          inputVal = inputVal.split(",")[0];
+          content = el
+            .querySelector(".city-name span")
+            .textContent.toLowerCase();
+        } else {
+          content = el.querySelector(".city-name").dataset.name.toLowerCase();
+        }
+      } else {
+        
+        content = el.querySelector(".city-name span").textContent.toLowerCase();
+      }
+      return content == inputVal.toLowerCase();
+    });
+
+    if (filteredArray.length > 0) {
+      msg.textContent = `You already know the weather for ${
+        filteredArray[0].querySelector(".city-name span").textContent
+      }`;
+      
+      form.reset();
+      input.focus();
+      return;
+    }
+  }
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const { main, name, sys, weather } = data;
+      const icon = `https://openweathermap.org/img/wn/${weather[0]["icon"]}@2x.png`;
+      let li = document.createElement("li");
+      li.classList.add("city");
+      const markup = `
+        <h2 class="city-name" data-name="${name},${sys.country}">
+          <span>${name}</span>
+          <sup>${sys.country}</sup>
+        </h2>
+        <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
+        <figure>
+          <img class="city-icon" src="${icon}" alt="${
+        weather[0]["description"]
+      }">
+          <figcaption>${weather[0]["description"]}</figcaption>
+        </figure>
+      `;
+      li.innerHTML = markup;
+      list.appendChild(li);
+    })
+    .catch(() => {
+      msg.textContent = "Location not found";
+    });
+
+  msg.textContent = "";
+  form.reset();
+  input.focus();
+});
